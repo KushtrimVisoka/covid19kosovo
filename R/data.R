9ef1fc663188979dba4d4ed1d9c9adce5074c7d3
Kushtrimvisoka <kushtrim.visoka@gmail.com>
2020-12-09 16:23:10 +0100

    Update

--- R/data.R
+++ R/data.R
@@ -7,16 +7,27 @@
 #'
 #' @examples
 covid19kosovo <- function(level = c("total", "municipality", "village")){
+
   level <- rlang::arg_match(level)
+
   if (level == "total"){
+    if (file.exists(data)){
+      file.remove(data)
+    }
     data <- utils::read.csv("https://kushtrimvisoka.github.io/datasets/covid19kosovo_timeseries.csv") %>%
       dplyr::select(-1)
     return(data)
   } else if (level == "municipality"){
+    if (file.exists(data)){
+      file.remove(data)
+    }
     data <- utils::read.csv("https://kushtrimvisoka.github.io/datasets/covid19kosovo_timeseries_municipality.csv") %>%
       dplyr::select(-1)
     return(data)
   } else if (level == "village"){
+    if (file.exists(data)){
+      file.remove(data)
+    }
     data <- utils::read.csv("https://kushtrimvisoka.github.io/datasets/covid19kosovo_timeseries_cz.csv") %>%
       dplyr::select(-1)
     return(data)
